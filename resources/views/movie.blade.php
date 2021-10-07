b0fd3ffe83cfa89bf1fa0af5645cbc494ffa6cf8
Riki420 <girottiriccardo1998@gmail.com>
2021-10-07 18:09:32 +0200

    style

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,18 +1,25 @@
 @extends('layouts.main')
 @section('title', 'MovieDB')
 @section('content')
-     <div class="container">
-            <h1 class="text-center">
-                Movies
-            </h1>
-            <hr>
-            
+     <div class="container-fluid bg-dark">
+         <div class="row">
+             <div class="col">
+                <h1 class="text-center pt-2 text-white">
+                    Movies
+                </h1>
+             </div>
+         </div>
+        </div>
+        <div class="container">
             <div class="row justify-content-center">
                @forelse ($movies as $movie)
-                <div class="col-4 m-4" style="width: 18rem;">
-                    <div class="film-detail">
-                        <h3>{{ $movie['title'] }}   </h3>
-                        <h5 class="mb-4 text-muted">Titolo Originale:{{ $movie['original_title'] }}</h5>
+                <div class="col-4 m-4 align-items-stretch">
+                    <div class="film-detail border">
+                        <div class="title bg-dark text-white text-center p-2">
+                            <h3>{{ $movie['title'] }}</h3>
+                        </div>
+                        <h5 class="pb-4 text-muted text-center pt-4">Titolo Originale:{{ $movie['original_title'] }}</h5>
+                        <hr>
                         <ul>
                             <li>Paese di Produzione: {{ $movie['nationality'] }}</li>
                             <li>Data di uscita: {{ $movie['date'] }}</li>
